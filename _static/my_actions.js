b30970c4dc59814af20ab7089d59921c097adc00
Eduard Staniloiu <edi33416@gmail.com>
2020-08-14 01:43:49 +0300

    Fix toggle sidebar chapters sections
    Signed-off-by: Eduard Staniloiu <edi33416@gmail.com>

--- _static/my_actions.js
+++ _static/my_actions.js
@@ -1,5 +1,34 @@
-$( document ).ready(function() {
+$(function() {
+    var toggleCaptionVisibility = function(idx, elem) {
+        //var tocCaption = this.innerText.split("\n")[0];
+        var tocCaption = elem.innerText.split("\n")[0];
+
+        var visibleCaption = sessionStorage.getItem("visibleCaption");
+        if (visibleCaption) {
+            //var isVisible = $(this).children("ul").is(":visible");
+            var isVisible = $(elem).children("ul").is(":visible");
+            if (((!isVisible) && (visibleCaption === tocCaption)) ||
+                (isVisible && (visibleCaption !== tocCaption))) {
+
+                $(elem).children("ul").toggle();
+            }
+        }
+    };
+
+    if (sessionStorage.getItem("visibleCaption")) {
+        $(".sidebar-group").each(toggleCaptionVisibility);
+    }
+
+    $(".sidebar-group .caption-text a").click(function(event) {
+        event.preventDefault();
+    });
+
     $(".sidebar-group").click(function() {
         $(this).children("ul").toggle();
+
+        var tocCaption = this.innerText.split("\n")[0];
+        sessionStorage.setItem("visibleCaption", tocCaption);
+
+        $(".sidebar-group").each(toggleCaptionVisibility);
     });
 });
